8125b6b00e4335b349210931db2438a6de30696e
arijitk930 <arijitk930@gmail.com>
2024-07-31 19:43:11 +0530

    final committ

--- src/index.js
+++ src/index.js
@@ -93,13 +93,14 @@ function Menu() {
 }
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
+
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : " "}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}</span>
       </div>
     </li>
   );
